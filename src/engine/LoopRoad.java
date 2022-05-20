470faad13b7906be5ebf03f8f08e202f37e9965f
Niko Dalla Noce <dllnik97@gmail.com>
2022-05-20 17:02:25 +0200

    pretty working

--- src/engine/LoopRoad.java
+++
@@ -1,9 +0,0 @@
-package engine;
-
-public class LoopRoad extends YCross{
-    private Road incoming;
-    public LoopRoad(int capacity, Road outgoing, Road incoming) {
-        super(capacity, outgoing);
-        this.incoming = incoming;
-    }
-}
