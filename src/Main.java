110845b3d5a8a4143c26c56653855840968ee039
Niko Dalla Noce <dllnik97@gmail.com>
2022-05-25 17:19:44 +0200

    scenarios work fine

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,13 @@ import java.util.List;
 
 public class Main {
 
+    int recInterval = 10;
+    static List<Metric<Straight, Double>> metrics = new LinkedList<>();
+
     private static void overtakeScenario(int runs) throws Exception {
         //Build roads
-        Road firstStraight = new Straight(3, 10, 0, 2, 0.05, 1, 1, null, null);
-        Road secondStraight = new Straight(3, 10, 0, 5, 0.05, 1, 1, null, null);
+        Road firstStraight = new Straight(3, 10, 0, 2, 0.05, 1, 1, null, metrics);
+        Road secondStraight = new Straight(3, 10, 0, 5, 0.05, 1, 1, null, metrics);
         Road firstCross = new YCross(3, secondStraight);
         Road secondCross = new YCross(2, firstStraight);
         firstStraight.setOutgoing(firstCross);
@@ -35,7 +38,7 @@ public class Main {
     }
 
     private static void pingPongScenario(int runs) throws Exception {
-        Road road = new Straight(3, 10, 0, 3, 0.05, 1, 1, new DeadEndRoad(), null);
+        Road road = new Straight(3, 10, 0, 3, 0.05, 1, 1, new DeadEndRoad(), metrics);
 
         //Fill road
         for (int i=0;i<8;i++) {
@@ -49,12 +52,6 @@ public class Main {
 
     private static void randomScenario(int runs) throws Exception {
         //Define the metrics
-        int recInterval = 10;
-        List<Metric<Straight, Double>> metrics = new LinkedList<>();
-        metrics.add(new AvgSpeed(recInterval));
-        metrics.add(new Density(recInterval));
-        metrics.add(new Flow(recInterval));
-        metrics.add(new ChangesOfLane(recInterval));
 
         //Build roads
         Road firstStraight = new Straight(3, 50, 0.5, 5, 0.1, 0.5, 1, null, metrics);
@@ -72,58 +69,34 @@ public class Main {
     }
 
     public static void intersectionScenario(int runs) throws Exception {
-        //Define the metrics
-        int recInterval = 10;
-        List<Metric<Straight, Double>> metrics = new LinkedList<>();
-        metrics.add(new ChangesOfLane(recInterval));
 
         //Build the roads
         Road firstStraight = new Straight(5, 50, 0.5, 5, 0.1, 0.5, 1, null, metrics);
         Road secondStraight = new Straight(2, 50, 0.5, 5, 0.1, 0.5, 1, null, metrics);
-        Road prova = new Straight(1, 10, 0.5, 5, 0.1, 0.5, 1, null, metrics);
+        Road intersection = new Straight(1, 10, 0.5, 5, 0.1, 0.5, 1, null, metrics);
         Road firstCross = new YCross(5, secondStraight);
         Road secondCross = new YCross(2, firstStraight);
         firstStraight.setOutgoing(firstCross);
         secondStraight.setOutgoing(secondCross);
-        prova.setOutgoing(secondCross);
+        intersection.setOutgoing(secondCross);
 
         //Run the scenario
         var thr1 = new RoadsUpdater(firstCross, secondStraight);
         var thr2 = new RoadsUpdater(secondCross, firstStraight);
-        var thr3 = new RoadsUpdater(prova);
+        var thr3 = new RoadsUpdater(intersection);
         Scenario scenario = new Scenario(0, thr1, thr2, thr3);
         scenario.run(runs, true);
     }
 
-    private static void changeOfLanesMetric() throws Exception {
-        int recInterval = 10;
-        int[] lanes = {2, 3, 4, 5};
-        double[] densities = {0.1, 0.3, 0.5, 0.8};
-        for (int lane: lanes) {
-            for (double density: densities) {
-                double average_changes = 0;
-                System.out.println("Lanes: " + lane + ", Density: " + density);
-                for (int i=0;i<5;i++) {
-                    List<Metric<Straight, Double>> metrics = new LinkedList<>();
-                    metrics.add(new ChangesOfLane(recInterval));
-                    Road firstStraight = new Straight(lane, 50, density, 5, 0.1, 0.5, 1, null, metrics);
-                    Road firstCross = new YCross(lane, firstStraight);
-                    firstStraight.setOutgoing(firstCross);
-                    Scenario scenario = new Scenario(firstStraight, 1, 0);
-                    scenario.run(1000, false);
-                    double changesLane = firstStraight.changesOfLane();
-                    average_changes += changesLane / 1000;
-                    System.out.println("Run "+ i +", " + "Changes of lane: " + changesLane + ", AverageChanges: " + changesLane/1000);
-                }
-                average_changes /= 5;
-                System.out.println("Average Changes of lane configuration: " + average_changes + "\n");
-            }
-        }
-    }
 
     public static void main(String[] args) {
         int runs_first_scenarios = 5;
         int runs_second_scenarios = 10;
+        int recInterval = 10;
+        metrics.add(new AvgSpeed(recInterval));
+        metrics.add(new Density(recInterval));
+        metrics.add(new Flow(recInterval));
+        metrics.add(new ChangesOfLane(recInterval));
         try {
             overtakeScenario(runs_first_scenarios);
             //pingPongScenario(runs_first_scenarios);
